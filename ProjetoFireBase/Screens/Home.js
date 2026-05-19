72724f98965f9f09e347ab2d56cfc0a13c70fd55
Arthur Agenor Mezari Gomes <arthurmezari81@gmail.com>
2026-05-19 17:25:25 -0300

    Bancodedados1

--- ProjetoFireBase/Screens/Home.js
+++ ProjetoFireBase/Screens/Home.js
@@ -1,19 +1,44 @@
-import { View, Text, StyleSheet, Image, ScrollView, Button } from 'react-native';
-
-
+import { View, Text, StyleSheet, Image, ScrollView, Button, FlatList } from 'react-native';
+import {useState, useEffect} from "react";
+import { database } from '../FirebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 
 
 export default function Home({ navigation }) {
 
     // const motorista = drivers[0]
+    const [produtos, setProdutos] = useState([]);
 
+    useEffect(() => {
+        async function carregarProdutos() {
+            try {
+                const querySnapshot = await getDocs(collection(database, 'produtos'));
+                const lista = []
+                querySnapshot.forEach((doc) => {
+                    lista.push({id: doc.id, ...doc.data()})
+                })
+                setProdutos(lista)
+            } catch (error) {
+                console.log('Erro ao buscar no banco', error)
+            }
+        }
+        carregarProdutos();
+    })
     return (
 
         <View>
 
             <Text style={styles.txt}>Nome</Text>
-
+            <FlatList
+            data={produtos}
+            renderItem={({item}) => (
+                <View>
+                    <Text>
+                        
+                    </Text>
+            )}
+            }
             <Button title="Add Produto" color="#000"
                 onPress={() => navigation.navigate('Add Produtos')} />
         </View>
